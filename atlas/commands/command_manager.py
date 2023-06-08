10a6a2eaf894e19d736883cfe675ef1253e9cc14
alex paden <padenalex0@gmail.com>
2023-06-07 20:13:57 -0500

    simple bot framework for multiple command types

---
+++ atlas/commands/command_manager.py
@@ -0,0 +1,109 @@
+import logging
+import os
+
+from farcaster.models import Parent
+
+from atlas.commands.hash import Hash
+
+DEV_MODE = bool(os.getenv("DEV_MODE") == "True")
+HASH_COM = "hash"  # This is an example command not meant for production
+HELP_COM = "help"
+
+
+class Commands:
+    def __init__(self, fcc, bot_username):
+        self.fcc = fcc
+        self.bot_username = bot_username
+        self.hash = Hash(fcc)
+
+    def handle_command(self, notif):
+        command_mapping = {
+            HASH_COM: self.handle_hash_command,
+            HELP_COM: self.handle_help_command,
+        }
+
+        command_prefix = f"{self.bot_username} "
+        for command, handler in command_mapping.items():
+            if notif.content.cast.text.lower().startswith(command_prefix + command):
+                handler(notif)
+                break
+
+    def handle_generic_command(self, notif, command, perform_func):
+        if self.should_command_run(
+            notif.content.cast.hash,
+            command,
+            notif.content.cast.author.username,
+        ):
+            try:
+                perform_func(notif)
+                self.mark_command_run(notif.content.cast.hash)
+            except Exception as e:
+                self.handle_error(e, f"Error while handling {command} command")
+
+    def handle_hash_command(self, notif):
+        self.handle_generic_command(notif, HASH_COM, self.perform_hash_command)
+
+    def handle_help_command(self, notif):
+        self.handle_generic_command(notif, HELP_COM, self.perform_help_command)
+
+    def should_command_run(self, hash: str, type: str, username: str):
+        try:
+            # update to check if bot has liked cast
+            # TODO
+            pass
+        except Exception as e:
+            self.handle_error(e, "Error while checking if command should run: {hash}")
+            return False
+
+    def mark_command_run(self, hash: str):
+        try:
+            if DEV_MODE:
+                logging.info("Marking command as run (but dev mode)")
+            else:
+                # update to use like cast
+                # TODO
+                pass
+        except Exception as e:
+            self.handle_error(e, "Error while marking command as run")
+
+    def post_to_farcaster(self, text: str, parent: Parent):
+        try:
+            if DEV_MODE:
+                logging.info("Posting to farcaster (but dev mode)")
+            else:
+                self.fcc.post_cast(text=text, parent=parent)
+        except Exception as e:
+            self.handle_error(e, "Error while posting to farcaster")
+
+    def post_thread_to_farcaster(self, replies: list, parent: Parent):
+        try:
+            if DEV_MODE:
+                logging.info("Posting to farcaster (but dev mode)")
+            else:
+                for reply in replies:
+                    res = self.fcc.post_cast(text=reply, parent=parent)
+                    parent = res.cast.parent_hash
+        except Exception as e:
+            self.handle_error(e, "Error while posting to farcaster")
+
+    # This is a generic command example not meant for production
+    def perform_hash_command(self, notif):
+        logging.info("Performing hash command")
+        reply, parent = self.hash.start_hash(notif.content.cast)
+        self.post_to_farcaster(text=reply, parent=parent)
+        logging.info("Hash command completed")
+
+    def perform_help_command(self, notif):
+        logging.info("Performing help command")
+        reply = (
+            "Thanks for your interest in atlas bot! "
+            "Tag @alexpaden for further assistance. "
+            # "https://i.imgur.com/XuSem8Q.png"
+        )
+        parent = Parent(fid=notif.content.cast.author.fid, hash=notif.content.cast.hash)
+        self.post_to_farcaster(text=reply, parent=parent)
+        logging.info("Help command completed")
+
+    def handle_error(self, error, message):
+        # Log the error and take any necessary action to handle it
+        logging.error(f"{message}: {error}")
---
+++ atlas/main.py
@@ -0,0 +1,61 @@
+import logging
+import os
+import time
+
+# import openai
+from dotenv import load_dotenv
+from farcaster import Warpcast
+
+from atlas.commands.command_manager import Commands
+
+load_dotenv()
+
+
+def configure_main_function():
+    logging.basicConfig(
+        filename="bot.log",
+        level=logging.ERROR,
+        format="%(asctime)s %(message)s",
+    )
+
+    fcc = Warpcast(access_token=os.getenv("FARC_SECRET"))
+    # openai.api_key = os.getenv("OPENAI_KEY")
+    bot_username = os.getenv("USERNAME")
+
+    return Commands(
+        fcc=fcc,
+        bot_username=bot_username,
+    )
+
+
+def start_notification_stream(commands_instance: Commands):
+    fcc = commands_instance.fcc
+    bot_username = commands_instance.bot_username
+
+    for notif in fcc.stream_notifications():
+        if notif and notif.content.cast.text.startswith(bot_username):
+            commands_instance.handle_command(notif)
+
+
+def main():
+    logging.info("Configuring main function")
+    try:
+        commands_instance = configure_main_function()
+    except Exception as e:
+        logging.error(f"Error occurred configuring main function: {e}")
+        return
+
+    logging.info("Starting notification stream in main")
+    while True:
+        try:
+            start_notification_stream(commands_instance)
+        except Exception as e:
+            logging.error(f"Error occurred in notification stream, main function: {e}")
+            time.sleep(60)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logging.error(f"Error occurred __main__ function: {e}")
